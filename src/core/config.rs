c1987d26858b2368583fe6f56c18a2682843037a
subh <subhajitchaudhury05@gmail.com>
2026-04-11 03:43:53 +0200

    core: implement json file deserialization

--- src/core/config.rs
+++ src/core/config.rs
@@ -18,3 +18,26 @@ pub struct SoundPack {
 
 impl SoundPack {
     pub fn parse_config_file(sound_type: &SoundFiles, debug: bool) -> Result<Self, Box<dyn Error>> {
+        let sound_dir: PathBuf = FILE_PATH.join(SoundFiles::get_extract_dir(sound_type));
+        if debug {
+            println!("Reading config in: {:?}", sound_dir);
+        }
+
+        let config_file: PathBuf = sound_dir.join("config.json");
+        let json_string: String = fs::read_to_string(&config_file)
+            .map_err(|err: io::Error| EchoErrors::JSONReadError { err })?;
+
+        if debug {
+            println!("Config file content: {}", json_string);
+        }
+
+        let data: SoundPack = match serde_json::from_str(&json_string) {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("JSON parsing error: {:?}", err);
+                return Err(Box::new(EchoErrors::JSONParseError { err }));
+            }
+        };
+        Ok(data)
+    }
+}
