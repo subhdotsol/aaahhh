97e3cf4f8d719017ed8bdd05d6f94da3c9ad0d3c
subh <subhajitchaudhury05@gmail.com>
2026-04-11 02:29:21 +0200

    fix: implement Display standard trait for EchoErrors

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -18,3 +18,31 @@ impl Display for EchoErrors {
             EchoErrors::CouldNotCreateDataDirectory { err } => {
                 writeln!(f, "Error creating data directory: {}", err)
             }
+            EchoErrors::UnableToPlayFile { err } => {
+                writeln!(f, "Unable to play file: {}", err)
+            }
+            EchoErrors::UnableToDownloadFile { err } => {
+                writeln!(f, "Error: Downloading Sound File: {}", err)
+            }
+            EchoErrors::UnwantedSelectionIndex { index } => {
+                writeln!(f, "Error: Unwanted Selection: {}", index)
+            }
+            EchoErrors::UnzipError { err } => {
+                writeln!(f, "Error: Couldn't extract zip: {}", err)
+            }
+            EchoErrors::RemoveFileError { err } => {
+                writeln!(f, "Error: Couldn't remove zip file: {}", err)
+            }
+            EchoErrors::JSONReadError { err } => {
+                writeln!(f, "Error: Couldn't read json file: {}", err)
+            }
+            EchoErrors::JSONParseError { err } => {
+                writeln!(f, "Error: Couldn't parse json file: {}", err)
+            }
+        }
+    }
+}
+
+impl Error for EchoErrors {}
+
+// Sys constraints applied
